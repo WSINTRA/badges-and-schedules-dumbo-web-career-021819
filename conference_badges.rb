7ef7b5a9bce0bfddbae73638c53c76024cc9237a
William Robert Simpson <will.simpson85@gmail.com>
2019-01-17 21:16:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,7 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 def assign_rooms(array_of_names)
-  room_list = [1,2,3,4,5,6,7]
+  #room_list = [1,2,3,4,5,6,7]
   room_count = 0
   room_assign = []
   array_of_names.each {|x|
@@ -22,6 +22,12 @@ def assign_rooms(array_of_names)
 room_assign
 end
 
-def printer
+def printer(list_of_names)
+  badges = []
+  rooms = []
+  badges.push(batch_badge_creator(list_of_names))
+  rooms.push(assign_rooms(list_of_names))
+  puts badges
+  puts rooms  
 end
 #puts(assign_rooms(["jill","Bill","Dill"]))
