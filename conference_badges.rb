eec6dd2d474392b6e3d76b2b65da47a7569fd78d
William Robert Simpson <will.simpson85@gmail.com>
2019-01-17 20:55:11 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,5 @@ def batch_badge_creator(array_of_names)
   names_msg = []
 array_of_names.each {|x|names_msg.push("Hello, my name is #{x}")
   }
-puts(names_msg)
 end
 
-batch_badge_creator(["jill","Bill"])
